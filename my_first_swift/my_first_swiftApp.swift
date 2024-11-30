95b37d9bb1fc9265a880d44c0cb798f2b102ecf9
LongThav_SiPav <sipav.longthav@amkcambodia.com>
2024-11-30 09:01:34 +0700

    Initial Commit

---
+++ my_first_swift/my_first_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_swiftApp.swift
+//  my_first_swift
+//
+//  Created by Longthav Sipav on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct my_first_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my_first_swiftTests/my_first_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_swiftTests.swift
+//  my_first_swiftTests
+//
+//  Created by Longthav Sipav on 30/11/24.
+//
+
+import Testing
+@testable import my_first_swift
+
+struct my_first_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
